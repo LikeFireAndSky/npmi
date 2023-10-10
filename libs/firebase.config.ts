68c810ee15267359133a0ac98d6d99bdcaa1a7e3
MinSeob <icdmiracle927@gmail.com>
2023-10-11 01:11:42 +0900

    Feat : 환경변수 임명 방법 수정

--- libs/firebase.config.ts
+++ libs/firebase.config.ts
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: process.env.FIREBASE_APIKEY,
-	authDomain: process.env.FIREBASE_AUTHDOMAIN,
+	authDomain: 'https://npmi.vercel.app/',
 	projectId: process.env.FIREBASE_PROJECTID,
 	storageBucket: process.env.FIREBASE_STORAGEBUCKET,
 	messagingSenderId: process.env.FIREBASE_MESSAGINSENDERID,
